0fae33b006e09673d1718013d6ad406a695c5399
dibkb <dibas9110@gmail.com>
2023-03-10 18:41:10 +0530

    server running on port 🚀

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log("Working 🍾");
+import express from "express";
+import http from "http";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import config from "config";
+// ================================================
+const PORT = config.get<number>("port");
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
+});
